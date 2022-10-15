6696c56579506ea2ef33592ea430e9d74ac32f20
Kristina Kudryavtseva <chkud03@gmail.com>
2022-10-15 15:09:38 +0200

    Guessing game based on binary search

--- myprogram.py
+++ myprogram.py
@@ -1,17 +1,28 @@
 import random
 
-def  guess_number_game():
+def  guess_number_game(start, stop):
 
     won = False
-    number = random.randint(1, 10)
+    number = random.randint(start, stop)
 
     while (not won):
-        guess = input("Guess the nubmer from 1 to 10:_")
+        guess = input("Guess a nubmer from "+str(start)+" to "+str(stop)+":_")
         while not guess.isdigit():
             guess = input("Error - please input a number: ")
 
-        if number == int(guess):
+        guess = int(guess)
+
+        if number < guess:
+            print("The secret number is less than ", guess)
+            guess_number_game(start, guess)
+
+        elif number > guess:
+            print("The secret number is greater than ", guess)
+            guess_number_game(guess, stop)
+
+        elif number == guess:
             print("Congratulations - you won! The secret number is", number)
             won = True
+    return
 
-guess_number_game()
+guess_number_game(1, 10)
